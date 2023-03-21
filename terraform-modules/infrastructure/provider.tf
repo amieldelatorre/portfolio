d76a06181984b5ef6cb3291ea85dfbf17d4a7141
amieldelatorre <amieljames.delatorre@gmail.com>
2023-03-22 10:59:01 +1300

    Assumer role

--- terraform-modules/infrastructure/provider.tf
+++ terraform-modules/infrastructure/provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region   = "ap-southeast-2"
-  role_arn = "arn:aws:iam::778196150762:role/TerraformInCodeBuild"
+  assume_role {
+    role_arn = "arn:aws:iam::778196150762:role/TerraformInCodeBuild"
+  }
 
   default_tags {
     tags = local.tags
@@ -10,7 +12,10 @@ provider "aws" {
 provider "aws" {
   alias    = "usea1"
   region   = "us-east-1"
-  role_arn = "arn:aws:iam::778196150762:role/TerraformInCodeBuild"
+
+  assume_role {
+    role_arn = "arn:aws:iam::778196150762:role/TerraformInCodeBuild"
+  }
 
   default_tags {
     tags = local.tags
