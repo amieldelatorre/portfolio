8e7b232160c3af4a4f4b03e90a406ce87c819a99
amieldelatorre <amieljames.delatorre@gmail.com>
2023-03-22 13:49:32 +1300

    Fix s3 bucket permissons

--- terraform-modules/pipeline/main.tf
+++ terraform-modules/pipeline/main.tf
@@ -8,6 +8,23 @@ resource "aws_s3_bucket" "codepipeline_bucket" {
   bucket = "deployment-pipeline-bucket-${random_string.bucket_random_suffix.result}"
 }
 
+resource "aws_s3_bucket_ownership_controls" "codepipeline_bucket_ownership" {
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_public_access_block" {
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.codepipeline_bucket.id
   acl    = "private"
